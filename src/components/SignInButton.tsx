9bb71842f8444f105aeddcfc9c2e76fc5e62a1ac
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2023-10-24 13:30:26 +0300

    Fullwidth button

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -1,13 +1,13 @@
-import Button from 'payload/dist/admin/components/elements/Button'
-import React from 'react'
-import { SIGN_IN_PATH } from '../config'
+import Button from "payload/dist/admin/components/elements/Button";
+import React from "react";
+import { SIGN_IN_PATH } from "../config";
 
 export default function SignInButton() {
   return (
-    <div style={{ marginBottom: 40 }}>
-      <Button el="anchor" url={SIGN_IN_PATH}>
+    <div style={{ width: "100%" }}>
+      <Button style={{ width: "100%" }} el="anchor" url={SIGN_IN_PATH}>
         Sign in with your identity
       </Button>
     </div>
-  )
+  );
 }
